be762f3aea282a18f5e65c20de7c737f246f5b05
Sophia <sophiatu217@gmail.com>
2022-04-29 16:08:34 -0400

    Typo fixed in download.sh

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@ kaggle datasets download -d gianmarco96/upmcfood101
 # this unzips the zip file - you will get a directory named "data" containing the data
 unzip upmcfood101.zip
 # this cleans up the zip file, as we will no longer use it
-rm umcfood101.zip
+rm upmcfood101.zip
 # move to data folder
 mv images data
 
