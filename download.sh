7cb0b8b4fefac69b274c82fcf9db1f3edb6f484d
Sophia <sophiatu217@gmail.com>
2022-04-29 16:02:40 -0400

    Edited download.sh

--- download.sh
+++ download.sh
@@ -3,8 +3,11 @@
 #this downloads the zip file that contains the data
 kaggle datasets download -d gianmarco96/upmcfood101
 # this unzips the zip file - you will get a directory named "data" containing the data
-# unzip data.zip
+unzip upmcfood101.zip
 # this cleans up the zip file, as we will no longer use it
-# rm data.zip
+rm umcfood101.zip
+# move to data folder
+mkdir data
+mv upmcfood101 data
 
 echo downloaded data
