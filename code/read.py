a6ad9a10cb93ca8c5c4e262e6ee2a52164828476
Ian Acosta <ianacosta@ians-mbp.devices.brown.edu>
2022-05-06 01:40:13 -0400

    read all data

--- code/read.py
+++ code/read.py
@@ -14,27 +14,28 @@ print("Reading data...")
 count = 0
 for subdir, dirs, files in os.walk("../../../meerakurup/images/"):
     for file in files:
-        label = subdir.split('/')[-1]
-        labels.append(label)
-        i = Image.open(os.path.join(subdir, file))
-        i.thumbnail(maxsize, Image.ANTIALIAS)
-        
-        width, height = i.size   # Get dimensions
-        left = (width - img_len)/2
-        top = (height - img_len)/2
-        right = (width + img_len)/2
-        bottom = (height + img_len)/2
+        if file != ".DS_Store":
+            label = subdir.split('/')[-1]
+            labels.append(label)
+            i = Image.open(os.path.join(subdir, file))
+            i.thumbnail(maxsize, Image.ANTIALIAS)
+            
+            width, height = i.size   # Get dimensions
+            left = (width - img_len)/2
+            top = (height - img_len)/2
+            right = (width + img_len)/2
+            bottom = (height + img_len)/2
 
-        # Crop the center of the image
-        i_arr = np.array(i.crop((left, top, right, bottom)))
-        # print(i_arr.shape)
-        if len(i_arr.shape) != 3 or i_arr.shape[2] != 3:
-            i_arr = np.stack((i_arr[0],)*3, axis=-1)
-            print(os.path.join(subdir, file))
-        images.append(i_arr)
-        # print(label, np.array(i))
-        i.close()
-        count += 1
+            # Crop the center of the image
+            i_arr = np.array(i.crop((left, top, right, bottom)))
+            # print(i_arr.shape)
+            if len(i_arr.shape) != 3 or i_arr.shape[2] != 3:
+                i_arr = np.stack((i_arr[0],)*3, axis=-1)
+                print(os.path.join(subdir, file))
+            images.append(i_arr)
+            # print(label, np.array(i))
+            i.close()
+            count += 1
     if count % 1000 == 0:
         print("Processed " + str(count) + " Images")
 print("Saving data...")
