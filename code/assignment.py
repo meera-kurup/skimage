6f26e2cab0e203d489e8088ca4b5b5b0f2c1f5de
Sophia <sophiatu217@gmail.com>
2022-05-07 14:17:00 -0400

    Separated autoencoder loss

--- code/assignment.py
+++ code/assignment.py
@@ -54,7 +54,11 @@ def train(model, train_inputs, train_labels):
             print(y_pred.shape)
             print(batch_labels.shape)
             # print(batch_labels.shape)
-            loss = model.loss(y_pred, batch_inputs)
+            if args.autoencoder:
+                loss = model.loss(y_pred, batch_inputs)
+            else:
+                loss = model.loss(y_pred, batch_labels)
+                
             model.loss_list.append(loss)
             # accuracy = model.accuracy(y_pred, batch_labels)
             # model.accuracy_list.append(accuracy)
