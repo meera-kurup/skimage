21d464bd438a6564fe6c111dbc3abb519282069d
Ian Acosta <ianacosta@ians-mbp.devices.brown.edu>
2022-05-07 13:03:16 -0400

    test

--- code/autoencoder.py
+++ code/autoencoder.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tensorflow as tf
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxUnpooling2D,  MaxPooling2D, GlobalAveragePooling2D, BatchNormalization, AveragePooling2D, Input
+from keras.layers import Dense, Dropout, Flatten, Conv2D,  MaxPooling2D, GlobalAveragePooling2D, BatchNormalization, AveragePooling2D, Input
 from keras.applications.inception_v3 import InceptionV3
 from keras.regularizers import l2
 
@@ -10,14 +10,15 @@ class Autoencoder(tf.keras.Model):
         self.image_size = image_size
         self.encoder = tf.keras.Sequential([
             Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(self.image_size,self.image_size,3)),
-            MaxPooling2D(pool_size=(2, 2)),
+            # MaxPooling2D(pool_size=(2, 2)),
             Conv2D(64, kernel_size=(3, 3), activation='relu'),
-            MaxPooling2D(pool_size=(2, 2))])
+            # MaxPooling2D(pool_size=(2, 2))
+            ])
         
         self.decoder = tf.keras.Sequential([
-            MaxUnpooling2D(pool_size=(2, 2)),
+            # MaxUnpooling2D(pool_size=(2, 2)),
             Conv2D(64, kernel_size=(3, 3), activation='relu'),
-            MaxUnpooling2D(pool_size=(2, 2)),
+            # MaxUnpooling2D(pool_size=(2, 2)),
             Conv2D(32, kernel_size=(3, 3), activation='relu')])
 
     @tf.function
