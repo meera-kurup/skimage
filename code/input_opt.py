740423228e2b91678144de9371012dab80d10207
Kelly Wang <kelly_wang1@brown.edu>
2022-05-08 16:08:45 -0400

    inputopt

--- code/input_opt.py
+++ code/input_opt.py
@@ -63,7 +63,10 @@ class InputOptimizer(tf.keras.Model):
             # print(out_numpy.shape)
             # ax.imshow(out_numpy, vmin = -1, vmax = 1)
             # ax.imshow((out_numpy * 255).astype(np.uint8))
+            out_numpy = np.clip(out_numpy, 0, 1)
+            # ax.imshow(out_numpy*255)
             ax.imshow(out_numpy)
+            
         self.opt_imgs += [self.fig2img(fig)]
         plt.close(fig)
 
