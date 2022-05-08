9ae8044ef15039e9366549a8486b67cdeb758bc7
Ian Acosta <ianacosta@ians-mbp.devices.brown.edu>
2022-05-08 13:04:55 -0400

    loss

--- code/assignment.py
+++ code/assignment.py
@@ -115,7 +115,9 @@ def view_autoencoder_results(inputs, model, num_classes, split):
         inputs[i*(1000-split)]
         np.expand_dims(inputs[i*(1000-split)], axis=0)
         model.call(np.expand_dims(inputs[i*(1000-split)], axis=0))
-        img = model.call(np.expand_dims(inputs[i*(1000-split)], axis=0))*255
+        img = model.call(np.expand_dims(inputs[i*(1000-split)], axis=0))
+        img = tf.transpose(img, perm=[0,2,3,1])
+        img = tf.reshape(img, (-1, 3, 128, 128))*255
         print(img)
         img = np.squeeze(img)
         fig.add_subplot(rows, columns, i)
